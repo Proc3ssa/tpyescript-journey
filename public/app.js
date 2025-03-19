5e359e307cd348f09360db0f81413f786a5fadba
Faisal <pros3sa@gmail.com>
2025-03-19 22:07:24 +0000

    DOM and type casting

--- public/app.js
+++ public/app.js
@@ -1,1 +1,13 @@
 "use strict";
+// const anchor = document.querySelector('a')!;
+// console.log(anchor.href);
+const form = document.querySelector('.new-item-form');
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const detail = document.querySelector('#detail');
+const amount = document.querySelector('#amount');
+;
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, detail.value, amount.value);
+});
