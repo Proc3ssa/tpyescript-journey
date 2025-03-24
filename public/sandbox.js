4fef80f93390598082ae6614555a31b78e65fa0c
Faisal <pros3sa@gmail.com>
2025-03-24 20:40:35 +0000

    still learning

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,12 +1,33 @@
 "use strict";
-let greet = (user, department) => {
-    console.log(`User is ${user.name} and ID is ${user.uid} and is at department ${department}`);
-};
-greet({
-    name: "faisal",
-    uid: "898898"
-}, "Computer Science");
-let woman;
-woman = (standard) => {
-    console.log(`She is class ${standard.clss} so she is ${standard.grade}`);
-};
+// type properties = {name: string, uid: number|string} //type aliases
+// let greet = (user: properties, department:string) =>{
+//     console.log(`User is ${user.name} and ID is ${user.uid} and is at department ${department}`)
+// }
+// greet({
+//     name:"faisal",
+//     uid:"898898"
+// }, "Computer Science")
+// // function signature
+// type data = {grade:string, clss: number}
+// let woman : (statndard:data) => void;
+// woman = (standard:data)=>{
+//    console.log(`She is class ${standard.clss} so she is ${standard.grade}`);
+// }
+//classes
+class Parent {
+    constructor(name, gender) {
+        this.name = name;
+        this.Gender = gender;
+    }
+    who() {
+        if (this.Gender == 'male') {
+            return "Father";
+        }
+        else {
+            return "Mother";
+        }
+    }
+}
+const parent1 = new Parent("Halid", "male");
+const parent2 = new Parent("Rahi", "female");
+alert(parent1.who);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,24 +1,57 @@
-type properties = {name: string, uid: number|string} //type aliases
+// type properties = {name: string, uid: number|string} //type aliases
 
-let greet = (user: properties, department:string) =>{
-    console.log(`User is ${user.name} and ID is ${user.uid} and is at department ${department}`)
+// let greet = (user: properties, department:string) =>{
+//     console.log(`User is ${user.name} and ID is ${user.uid} and is at department ${department}`)
     
+// }
+
+// greet({
+//     name:"faisal",
+//     uid:"898898"
+// }, "Computer Science")
+
+
+// // function signature
+
+// type data = {grade:string, clss: number}
+
+// let woman : (statndard:data) => void;
+
+// woman = (standard:data)=>{
+
+//    console.log(`She is class ${standard.clss} so she is ${standard.grade}`);
+// }
+
+//classes
+
+class Parent{
+
+    name: string;
+    Gender: string; 
+    
+    constructor(name:string, gender:string){
+        this.name=name;
+        this.Gender=gender
+
+    }
+
+
+
+
+    who(){
+        if(this.Gender == 'male'){
+            return "Father";
+        }
+        else{
+            return "Mother"
+        }
+    }
+
+   
 }
 
-greet({
-    name:"faisal",
-    uid:"898898"
-}, "Computer Science")
+const parent1 = new Parent("Halid", "male");
+const parent2 = new Parent("Rahi", "female");
 
-
-// function signature
-
-type data = {grade:string, clss: number}
-
-let woman : (statndard:data) => void;
-
-woman = (standard:data)=>{
-
-   console.log(`She is class ${standard.clss} so she is ${standard.grade}`);
-}
+alert(parent1.who)
 
